aa307aadec5821c0dfd2ef7bedf2fb5d084703ce
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-11-16 14:14:51 -0200

    [IMP] data/setup.py

--- data/setup.py
+++ data/setup.py
@@ -1328,6 +1328,38 @@ if __name__ == '__main__':
     # print('--> Executing jcafb_set_communities()...')
     # jcafb_set_communities(client)
 
+    # ***** 2016-11-16 *****
+    #
+
+    # print('-->', client)
+    # print('--> Executing jcafb_mass_editing_create()...')
+    # jcafb_mass_editing_create(client)
+
+    # db_path = 'data/clvhealth_jcafb_2017_2016-11-15a.sqlite'
+    # print('-->', client, db_path, conn_string)
+    # print('--> Executing jcafb_import_sqlite()...')
+    # jcafb_import_sqlite(client, db_path, conn_string)
+
+    # print('-->', client)
+    # print('--> Executing jcafb_set_communities()...')
+    # jcafb_set_communities(client)
+
+    # ***** odoo-mint18
+    #
+    # cd '/opt/openerp'
+    # pg_dump clvhealth_jcafb_dev -Fp -U postgres -h localhost -p 5432 > clvhealth_jcafb_dev_2016-11-16a.sql
+    # gzip clvhealth_jcafb_dev_2016-11-16a.sql
+    #
+    # cd '/opt/openerp'
+    # dropdb -i clvhealth_jcafb_dev
+    # createdb -O openerp -E UTF8 -T template0 clvhealth_jcafb_dev
+    # psql -f clvhealth_jcafb_dev_2016-11-16a.sql -d clvhealth_jcafb_dev -U postgres -h localhost -p 5432 -q
+    #
+    db_path = 'data/clvhealth_jcafb_2017_2016-11-16a.sqlite'
+    print('-->', client, db_path, conn_string)
+    print('--> Executing jcafb_export_sqlite()...')
+    jcafb_export_sqlite(client, db_path, conn_string)
+
     print()
     print('--> setup.py', '- Execution time:', secondsToStr(time() - start))
     print()
