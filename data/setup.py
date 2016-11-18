5365e26001d30955227c06cc5f90c5d8486e756c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-11-18 21:02:22 -0200

    [IMP] data/setup.py

--- data/setup.py
+++ data/setup.py
@@ -1533,11 +1533,36 @@ if __name__ == '__main__':
     # print('--> Executing jcafb_export_sqlite()...')
     # jcafb_export_sqlite(client, db_path, conn_string)
 
+    # print('-->', client)
+    # print('--> Executing jcafb_mass_editing_create()...')
+    # jcafb_mass_editing_create(client)
+
+    # db_path = 'data/clvhealth_jcafb_2017_2016-11-18b.sqlite'
+    # print('-->', client, db_path, conn_string)
+    # print('--> Executing jcafb_import_sqlite()...')
+    # jcafb_import_sqlite(client, db_path, conn_string)
+
+    # # ***** odoo-mint18
+    # #
+    # # cd '/opt/openerp'
+    # # pg_dump clvhealth_jcafb_dev -Fp -U postgres -h localhost -p 5432 > clvhealth_jcafb_dev_2016-11-18c.sql
+    # # gzip clvhealth_jcafb_dev_2016-11-18c.sql
+    # #
+    # # cd '/opt/openerp'
+    # # dropdb -i clvhealth_jcafb_dev
+    # # createdb -O openerp -E UTF8 -T template0 clvhealth_jcafb_dev
+    # # psql -f clvhealth_jcafb_dev_2016-11-18c.sql -d clvhealth_jcafb_dev -U postgres -h localhost -p 5432 -q
+    # #
+    # db_path = 'data/clvhealth_jcafb_2017_2016-11-18c.sqlite'
+    # print('-->', client, db_path, conn_string)
+    # print('--> Executing jcafb_export_sqlite()...')
+    # jcafb_export_sqlite(client, db_path, conn_string)
+
     print('-->', client)
     print('--> Executing jcafb_mass_editing_create()...')
     jcafb_mass_editing_create(client)
 
-    db_path = 'data/clvhealth_jcafb_2017_2016-11-18b.sqlite'
+    db_path = 'data/clvhealth_jcafb_2017_2016-11-18c.sqlite'
     print('-->', client, db_path, conn_string)
     print('--> Executing jcafb_import_sqlite()...')
     jcafb_import_sqlite(client, db_path, conn_string)
