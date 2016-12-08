621e2616d74b2db9837614e7f55c1a5c04726f04
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-12-08 19:25:27 -0200

    [IMP] data/setup.py

--- data/setup.py
+++ data/setup.py
@@ -2161,18 +2161,43 @@ if __name__ == '__main__':
     # print('--> Executing jcafb_import_sqlite()...')
     # jcafb_import_sqlite(client, db_path, conn_string)
 
+    # # ***** odoo-mint18
+    # #
+    # # cd '/opt/openerp'
+    # # pg_dump clvhealth_jcafb_dev -Fp -U postgres -h localhost -p 5432 > clvhealth_jcafb_dev_2016-12-08a.sql
+    # # gzip clvhealth_jcafb_dev_2016-12-08a.sql
+    # #
+    # # cd '/opt/openerp'
+    # # dropdb -i clvhealth_jcafb_dev
+    # # createdb -O openerp -E UTF8 -T template0 clvhealth_jcafb_dev
+    # # psql -f clvhealth_jcafb_dev_2016-12-08a.sql -d clvhealth_jcafb_dev -U postgres -h localhost -p 5432 -q
+    # #
+    # db_path = 'data/clvhealth_jcafb_2017_2016-12-08a.sqlite'
+    # print('-->', client, db_path, conn_string)
+    # print('--> Executing jcafb_export_sqlite()...')
+    # jcafb_export_sqlite(client, db_path, conn_string)
+
+    # print('-->', client)
+    # print('--> Executing jcafb_mass_editing_create()...')
+    # jcafb_mass_editing_create(client)
+
+    # db_path = 'data/clvhealth_jcafb_2017_2016-12-08a.sqlite'
+    # print('-->', client, db_path, conn_string)
+    # print('--> Executing jcafb_import_sqlite()...')
+    # jcafb_import_sqlite(client, db_path, conn_string)
+
     # ***** odoo-mint18
     #
     # cd '/opt/openerp'
-    # pg_dump clvhealth_jcafb_dev -Fp -U postgres -h localhost -p 5432 > clvhealth_jcafb_dev_2016-12-08a.sql
-    # gzip clvhealth_jcafb_dev_2016-12-08a.sql
+    # pg_dump clvhealth_jcafb_dev -Fp -U postgres -h localhost -p 5432 > clvhealth_jcafb_dev_2016-12-08b.sql
+    # gzip clvhealth_jcafb_dev_2016-12-08b.sql
     #
     # cd '/opt/openerp'
     # dropdb -i clvhealth_jcafb_dev
     # createdb -O openerp -E UTF8 -T template0 clvhealth_jcafb_dev
-    # psql -f clvhealth_jcafb_dev_2016-12-08a.sql -d clvhealth_jcafb_dev -U postgres -h localhost -p 5432 -q
+    # psql -f clvhealth_jcafb_dev_2016-12-08b.sql -d clvhealth_jcafb_dev -U postgres -h localhost -p 5432 -q
     #
-    db_path = 'data/clvhealth_jcafb_2017_2016-12-08a.sqlite'
+    db_path = 'data/clvhealth_jcafb_2017_2016-12-08b.sqlite'
     print('-->', client, db_path, conn_string)
     print('--> Executing jcafb_export_sqlite()...')
     jcafb_export_sqlite(client, db_path, conn_string)
