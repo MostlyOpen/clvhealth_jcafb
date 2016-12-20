1829bc8461372f5870bd9f0c1d39a99bfd3be30c
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2016-12-20 15:03:08 -0200

    [IMP] data/setup.py

--- data/setup.py
+++ data/setup.py
@@ -2503,6 +2503,21 @@ if __name__ == '__main__':
     # # psql -f clvhealth_jcafb_dev_2016-12-19c.sql -d clvhealth_jcafb_dev -U postgres -h localhost -p 5432 -q
     # #
 
+    # ***** 2016-12-20 *****
+    #
+
+    # # ***** odoo-mint18
+    # #
+    # # cd '/opt/openerp'
+    # # pg_dump clvhealth_jcafb_dev -Fp -U postgres -h localhost -p 5432 > clvhealth_jcafb_dev_2016-12-20a.sql
+    # # gzip clvhealth_jcafb_dev_2016-12-20a.sql
+    # #
+    # # cd '/opt/openerp'
+    # # dropdb -i clvhealth_jcafb_dev
+    # # createdb -O openerp -E UTF8 -T template0 clvhealth_jcafb_dev
+    # # psql -f clvhealth_jcafb_dev_2016-12-20a.sql -d clvhealth_jcafb_dev -U postgres -h localhost -p 5432 -q
+    # #
+
     # ***** Test *****
     #
 
